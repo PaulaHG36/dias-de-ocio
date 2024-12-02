e4349669eede89607a3625352a657e1ee35d0b55
Paula Mac <paula_hanxi_garcia_m-tsapp1ma2324@cev.com>
2024-12-02 17:01:01 +0000

    Se crea la función para actualizar los labels con los valores actuales y se ajusta el valor inicial de los sliders

--- ejercicio9/ViewController.swift
+++ ejercicio9/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  ejercicio9
-//
-//  Created by user236987 on 12/2/24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -16,10 +9,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var sleepLabel: UILabel!
     @IBOutlet weak var leisureLabel: UILabel!
     
+    let totalLifeYears: Double = 100
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // valor inicial de los sliders
+        workSlider.value = 8
+        sleepSlider.value = 8
+        
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        workLabel.text = "Horas de trabajo: \(Int(workSlider.value))"
+        sleepLabel.text = "Horas de sueño: \(Int(sleepSlider.value))"
     }
 
 
