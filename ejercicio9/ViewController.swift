83b1e21c5b09ecfa1d174a29b7b83686de36ba68
Paula Mac <paula_hanxi_garcia_m-tsapp1ma2324@cev.com>
2024-12-02 16:55:17 +0000

    Conectar los componentes de la interfaz en el código

--- ejercicio9/ViewController.swift
+++ ejercicio9/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var workSlider: UISlider!
+    @IBOutlet weak var sleepSlider: UISlider!
+    
+    @IBOutlet weak var workLabel: UILabel!
+    @IBOutlet weak var sleepLabel: UILabel!
+    @IBOutlet weak var leisureLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
